356b0d05569be5eec208378e9b754a9e5399c693
yulya.podolyako <podolyako.yulya@mail.ru>
2020-12-07 22:23:00 +0300

    Creete

---
+++ src/MainFrame.java
@@ -0,0 +1,153 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+public class MainFrame extends JFrame {
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+    private Double[] coefficients;
+    private JFileChooser fileChooser = null;
+
+    private JMenuItem saveToTextMenuItem;
+    private JMenuItem saveToGraphicsMenuItem;
+    private JMenuItem searchValueMenuItem;
+    private JTextField textFieldFrom;
+    private JTextField textFieldTo;
+    private JTextField textFieldStep;
+    private JMenuItem menuHelpAbout;
+    private Box hBoxResult;
+
+    public MainFrame(Double[] coefficients) {
+        super("Табулирование многочлена на отрезке по схеме Горнера");
+        this.coefficients = coefficients;
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH)/2, (kit.getScreenSize().height - HEIGHT)/2);
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+        JMenu tableMenu = new JMenu("Таблица");
+        menuBar.add(tableMenu);
+        JMenu aboutMenu = new JMenu("Справка");
+        menuHelpAbout =new JMenuItem("О программе");
+
+        aboutMenu.add(menuHelpAbout);
+        menuBar.add(aboutMenu);
+        menuBar.setBackground(Color.ORANGE);
+
+
+
+        JLabel labelForFrom = new JLabel("X изменяется на интервале от:");
+        textFieldFrom = new JTextField("0.0", 10);
+        textFieldFrom.setMaximumSize(textFieldFrom.getPreferredSize());
+        JLabel labelForTo = new JLabel("до:");
+        textFieldTo = new JTextField("1.0", 10);
+        textFieldTo.setMaximumSize(textFieldTo.getPreferredSize());
+        JLabel labelForStep = new JLabel("с шагом:");
+        textFieldStep = new JTextField("0.1", 10);
+        textFieldStep.setMaximumSize(textFieldStep.getPreferredSize());
+        Box hboxRange = Box.createHorizontalBox();
+        hboxRange.setBorder(BorderFactory.createBevelBorder(1));
+        hboxRange.setBackground(Color.ORANGE);
+        hboxRange.add(Box.createHorizontalGlue());
+        hboxRange.add(labelForFrom);
+        hboxRange.add(Box.createHorizontalStrut(10));
+        hboxRange.add(textFieldFrom);
+        hboxRange.add(Box.createHorizontalStrut(20));
+        hboxRange.add(labelForTo);
+        hboxRange.add(Box.createHorizontalStrut(10));
+        hboxRange.add(textFieldTo);
+        hboxRange.add(Box.createHorizontalStrut(20));
+        hboxRange.add(labelForStep);
+        hboxRange.add(Box.createHorizontalStrut(10));
+        hboxRange.add(textFieldStep);
+        hboxRange.add(Box.createHorizontalGlue());
+        hboxRange.setPreferredSize(new Dimension(new Double(hboxRange.getMaximumSize().getWidth()).intValue(),
+                new Double(hboxRange.getMinimumSize().getHeight()).intValue()*2));
+        getContentPane().add(hboxRange, BorderLayout.NORTH);
+        JButton buttonCalc = new JButton("Вычислить");
+        buttonCalc.setBackground(Color.YELLOW);
+        
+        JButton buttonReset = new JButton("Очистить поля");
+        buttonReset.setBackground(Color.YELLOW);
+        buttonReset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                textFieldFrom.setText("0.0");
+                textFieldTo.setText("1.0");
+                textFieldStep.setText("0.1");
+                hBoxResult.removeAll();
+                hBoxResult.add(new JPanel());
+                saveToTextMenuItem.setEnabled(false);
+                saveToGraphicsMenuItem.setEnabled(false);
+                searchValueMenuItem.setEnabled(false);
+                getContentPane().validate();
+            }
+        });
+        Box hboxButtons = Box.createHorizontalBox();
+        hboxButtons.setBorder(BorderFactory.createBevelBorder(1));
+        hboxButtons.add(Box.createHorizontalGlue());
+        hboxButtons.add(buttonCalc);
+        hboxButtons.setBackground(Color.orange);
+        hboxButtons.add(Box.createHorizontalStrut(30));
+        hboxButtons.add(buttonReset);
+        hboxButtons.add(Box.createHorizontalGlue());
+        hboxButtons.setPreferredSize(new Dimension(new
+                Double(hboxButtons.getMaximumSize().getWidth()).intValue(), new
+                Double(hboxButtons.getMinimumSize().getHeight()).intValue()*2));
+        getContentPane().add(hboxButtons, BorderLayout.SOUTH);
+        hBoxResult = Box.createHorizontalBox();
+        hBoxResult.add(new JPanel());
+        hBoxResult.setBackground(Color.ORANGE);
+        getContentPane().add(hBoxResult, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+        if (args.length==0) {
+            System.out.println("Невозможно табулировать многочлен, для которого не задано ни одного коэффициента!");
+            System.exit(-1);
+        }
+        Double[] coefficients = new Double[args.length];
+        int i = 0;
+        try {
+            for (String arg: args) {
+                coefficients[i++] = Double.parseDouble(arg);
+            }
+        }
+        catch (NumberFormatException ex) {
+            System.out.println("Ошибка преобразования строки '" + args[i] + "' в число типа Double");
+            System.exit(-2);
+        }
+        MainFrame frame = new MainFrame(coefficients);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
+
+
+
+
